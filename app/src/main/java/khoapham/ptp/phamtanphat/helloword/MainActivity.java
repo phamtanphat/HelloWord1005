21f738e22ba31b521246e7134009c20748f8c9f5
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-17 14:25:53 +0700

    a

---
+++ app/src/main/java/khoapham/ptp/phamtanphat/helloword/MainActivity.java
@@ -0,0 +1,21 @@
+package khoapham.ptp.phamtanphat.helloword;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //ctrl + P : to hop phim goi y tham so truyen vao
+        //Kiem tra du lieu ben trong system
+//        Log.d("BBB","Xin chao android");
+        //Thong bao ra giao dien
+//        Toast.makeText(MainActivity.this,"Xin chao",Toast.LENGTH_SHORT).show();
+    }
+}
